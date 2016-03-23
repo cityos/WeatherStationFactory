e58cafa8f75f35cde78a84d659e2a9249ab5c0e3
SaidSikira <saidsikira@gmail.com>
2016-03-23 14:29:32 +0100

    Initial framework project setup

---
+++ WeatherStationFactory/WeatherStationFactory.h
@@ -0,0 +1,19 @@
+//
+//  WeatherStationFactory.h
+//  WeatherStationFactory
+//
+//  Created by Said Sikira on 3/23/16.
+//  Copyright © 2016 CityOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeatherStationFactory.
+FOUNDATION_EXPORT double WeatherStationFactoryVersionNumber;
+
+//! Project version string for WeatherStationFactory.
+FOUNDATION_EXPORT const unsigned char WeatherStationFactoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherStationFactory/PublicHeader.h>
+
+
